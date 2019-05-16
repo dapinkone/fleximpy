784018ce51d31c5bab730813f1b827418506a03a
dap <dap@nagisa>
2019-05-16 01:45:16 -0700

    formatting with black. minor changes.

--- fleximpy.py
+++ fleximpy.py
@@ -1,24 +1,24 @@
 from kivy.app import App
 from kivy.uix.button import Button
-from kivy.uix.tabbedpanel import TabbedPanel, \
-    TabbedPanelItem, TabbedPanelHeader
+from kivy.uix.tabbedpanel import TabbedPanel, TabbedPanelItem, TabbedPanelHeader
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from flexproto import flexclient
 from kivy.uix.textinput import TextInput
 
+
 def callback(instance):
-    print('The button <%s> is being pressed' % instance.text)
+    print("The button <%s> is being pressed" % instance.text)
+
 
 class MainWin(App):
-
     def build(self):
         master_panel = TabbedPanel(do_default_tab=False)
 
-        connect_tab= TabbedPanelHeader(text="Connect")
+        connect_tab = TabbedPanelHeader(text="Connect")
         connectLayout = BoxLayout(padding=10, orientation="horizontal")
 
-        connect_label = Label(text=u'Connect to server:', font_size='20sp')
+        connect_label = Label(text=u"Connect to server:", font_size="20sp")
         connectLayout.add_widget(connect_label)
 
         serverTextbox = TextInput(text="")
@@ -30,16 +30,16 @@ class MainWin(App):
         serverTextbox.bind(on_text_validate=on_enter)
         connectLayout.add_widget(serverTextbox)
 
-        connect_tab.content=connectLayout
+        connect_tab.content = connectLayout
         master_panel.add_widget(connect_tab)
 
-        tab_header = TabbedPanelHeader(text='Roster')
+        tab_header = TabbedPanelHeader(text="Roster")
 
         rosterLayout = BoxLayout(padding=10, orientation="vertical")
-        title_label = Label(text=u'Hello world', font_size='20sp')
+        title_label = Label(text=u"Hello world", font_size="20sp")
         rosterLayout.add_widget(title_label)
         for b in range(10):
-            button = Button(text='Button ' + str(b))
+            button = Button(text="Button " + str(b))
             button.bind(on_press=callback)
             rosterLayout.add_widget(button)
         tab_header.content = rosterLayout
@@ -47,7 +47,9 @@ class MainWin(App):
 
         return master_panel
 
-class jklClient():
+
+class jklClient:
     roster = list()
-    
+
+
 MainWin().run()
--- flexproto.py
+++ flexproto.py
@@ -3,51 +3,68 @@ import nacl
 import msgpack
 from socket import create_connection
 from datetime import datetime as dt
+from time import sleep
+
 
 class Datum(Enum):
     Auth = 0
-    AuthResponse=1
-    Command=2
-    Message=3
-    Roster=4
-    User=5
+    AuthResponse = 1
+    Command = 2
+    Message = 3
+    Roster = 4
+    User = 5
 
-class flexclient():
-    def __init__(self, ip="10.20.30.14", port=4321, username="test123" ):
+
+class flexclient:
+    def __init__(self, ip="10.20.30.14", port=4321, username="test123"):
         self.username = username
         self.sock = create_connection((ip, port))
-        self.sock.send(msgpack.packb("FLEX")) # initial protocol header
-        d_type = Datum.Command
-        cmd = {"cmd" : "AUTH", "payload" : [username]}
+        self.sock.send(msgpack.packb("FLEX"))  # initial protocol header
+        cmd = {"cmd": "AUTH", "payload": [username]}
         self.send_datum(cmd, Datum.Command)
-        
-        challenge_d = self.read_datum()["data"]
+
+        _, challenge_d = self.read_datum()
         print(challenge_d)
         self.send_auth_response(challenge_d[b"challenge"])
-        d = self.read_datum()
-        print(d) 
+        self.request_roster()
+
+    def mainloop(self):
+        while True:
+            d_type, d_data = self.read_datum()
+            print(d_type, d_data)
+            if d_type == Datum.Roster:
+                self.roster = d_data
+
+    def request_roster(self):
+        self.send_datum({b"cmd": "ROSTER"}, Datum.Command)
 
     def send_auth_response(self, challenge):
-        self.send_datum({b"challenge":challenge}, Datum.AuthResponse)
+        self.send_datum({b"challenge": challenge}, Datum.AuthResponse)
 
-    def send_message(self, to, category="test", flags=["FLAGS?"], date=dt.now(), msgstr=""):
+    def send_message(
+        self, to, category="test", flags=["FLAGS?"], date=dt.now(), msgstr=""
+    ):
         msg = {
-            b"to" : to,
-            b"from" : self.username,
+            b"to": to,
+            b"from": self.username,
             b"flags": flags,
-            b"date" : date,
-            b"msg"  : msgstr,
+            b"date": date,
+            b"msg": msgstr,
         }
-        d_type = Datum.Message # type 3 = msg
+        d_type = Datum.Message  # type 3 = msg
         self.send_datum(msg, d_type)
 
     def send_datum(self, msg, d_type=0):
         out = msgpack.packb(msg, encoding="utf-8")
         size = len(out)
-        self.sock.send((d_type.value).to_bytes(1, "big") + size.to_bytes(2,"big") + out)
+        self.sock.send(
+            (d_type.value).to_bytes(1, "big") + size.to_bytes(2, "big") + out
+        )
 
     def read_datum(self):
-        d_type = int.from_bytes(self.sock.recv(1), "big") # datum type
-        d_size = int.from_bytes(self.sock.recv(2), "big") # length of msgpack data
-        datum  = msgpack.unpackb(self.sock.recv(d_size)) # TODO: proper exception handling
-        return {"d_type" : Datum(d_type), "data" : datum}
+        d_type = int.from_bytes(self.sock.recv(1), "big")  # datum type
+        d_size = int.from_bytes(self.sock.recv(2), "big")  # length of msgpack data
+        datum = msgpack.unpackb(
+            self.sock.recv(d_size)
+        )  # TODO: proper exception handling
+        return (Datum(d_type), datum)
