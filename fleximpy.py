bab4433b609913e9e3f349b21a1dbfaf48b2b54b
DaPinkOne <crystalshade@gmail.com>
2019-05-17 13:52:04 -0700

    progress being made

--- fleximpy.py
+++ fleximpy.py
@@ -5,33 +5,48 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from flexproto import flexclient
 from kivy.uix.textinput import TextInput
-
-
+from threading import Thread
+from binascii import hexlify, unhexlify
 
 
 class MainWin(App):
     def build(self):
         self.master_panel = TabbedPanel(do_default_tab=False)
-
+        self.users = dict()
+        # users data structure:
+        # users[key] = {"tab":tab, "outbox":outbox, "inbox", inbox}
         # set up the new connections tab
         connect_tab = TabbedPanelHeader(text="Connect")
-        connectLayout = BoxLayout(padding=10, orientation="horizontal")
+        connectLayout = BoxLayout(padding=10, orientation="vertical")
         connect_label = Label(text=u"Connect to server:", font_size="20sp")
         connectLayout.add_widget(connect_label)
-        serverTextbox = TextInput(text="")
+        
+        serverTextbox = TextInput(text="10.20.30.14")
         serverTextbox.multiline = False
 
-        def on_enter(instance): # callback for return key in the hostname box.
+        def on_enter(instance):  # callback for return key in the hostname box.
             self.flex = flexclient(ip=instance.text)
-
+            self.flex.got_message_callback = self.got_message_callback
             for name in self.flex.roster:
-                button = Button(text="Send Msg to " + str(name[b'aliases'][0]))
+                pub_key = name["key"]  # str(unhexlify(name['key']), encoding="utf-8")
+                try:
+                    alias = name["aliases"][0]
+                except IndexError:
+                    alias = "###"
+                if pub_key not in self.users.keys():
+                    self.users[pub_key] = {"key": pub_key, "alias": alias}
+                button = Button(text=alias)
                 button.bind(on_press=self.roster_click_callback)
                 self.rosterLayout.add_widget(button)
-            
+            self.master_panel.switch_to(self.tabs["Roster"])
 
         serverTextbox.bind(on_text_validate=on_enter)
         connectLayout.add_widget(serverTextbox)
+
+
+        usernameTextbox = TextInput(text="")
+        usernameTextbox.multiline = False
+        connectLayout.add_widget(usernameTextbox)
 
         connect_tab.content = connectLayout
         self.master_panel.add_widget(connect_tab)
@@ -43,30 +58,46 @@ class MainWin(App):
         title_label = Label(text=u"Hello world", font_size="20sp")
         self.rosterLayout.add_widget(title_label)
         tab_header.content = self.rosterLayout
+        self.roster = tab_header
         self.master_panel.add_widget(tab_header)
 
         return self.master_panel
 
-
     def roster_click_callback(self, instance):
         self.newChatTab(instance.text)
 
-    def newChatTab(self, target):
-                # set up the new connections tab
-            chat_tab = TabbedPanelHeader(text=target)
+    def newChatTab(self, target: str):
+        # set up a tab for each target user
+        if target in self.users.keys():
+            self.master_panel.switch_to(
+                self.users[target]["tab"]
+            )  # this, or master_panel.tab_list[0]?
+        else:  # build new tab, then swap focus.
+            chat_tab = TabbedPanelHeader(text=self.users[target]["alias"])
             chatLayout = BoxLayout(padding=10, orientation="horizontal")
-            chat_label = Label(text=u"Connect to server:", font_size="20sp")
-            chatLayout.add_widget(chat_label)
-            chatTextbox = TextInput(text="")
-            chatTextbox.multiline = False
+            chatOutput = TextInput(text="")
+            chatLayout.add_widget(chatOutput)
+            chatInput = TextInput(text="", size_hint=(0.2, None))
+            chatInput.multiline = False
 
-            def on_enter(instance): # callback for return key in the hostname box.
-                self.flex.send_message(message="hello?")
+            def on_enter(instance):  # callback for return key in the hostname box.
+                print("sending msg to: " + target)
+                self.flex.send_message(message=chatInput.text, to=target)
 
-            chatTextbox.bind(on_text_validate=on_enter)
-            chatLayout.add_widget(chatTextbox)
+            chatInput.bind(on_text_validate=on_enter)
+            chatLayout.add_widget(chatInput)
 
             chat_tab.content = chatLayout
+            self.users[target]["tab"] = chat_tab
+            self.users[target]["inbox"] = chatInput
+            self.users[target]["outbox"] = chatOutput
             self.master_panel.add_widget(chat_tab)
+
+    def got_message_callback(self, d_data):
+        target = d_data["from"]  # str(unhexlify(d_data['from']), encoding="utf-8")
+        self.newChatTab(target)
+        self.chatboxes[target].text += "\n>>>" + d_data["msg"]
+        pass
+
 
 MainWin().run()
--- flexproto.py
+++ flexproto.py
@@ -4,6 +4,9 @@ import msgpack
 from socket import create_connection
 from datetime import datetime as dt
 from time import sleep
+from typing import Callable, Iterable, Union, Optional, List
+from threading import Thread
+from binascii import hexlify
 
 
 class Datum(Enum):
@@ -13,61 +16,85 @@ class Datum(Enum):
     Message = 3
     Roster = 4
     User = 5
+    Status = 6
 
 
 class flexclient:
-    def __init__(self, ip="10.20.30.14", port=4321, username="test123"):
-        self.username = username
+    def __init__(
+        self,
+        ip: str = "10.20.30.14",
+        port: int = 4321,
+        username: str = "test" + str(dt.now()),
+    ) -> None:
+        self.pub_key = hexlify(bytes(username, encoding="utf-8"))
         self.sock = create_connection((ip, port))
         self.sock.send(msgpack.packb("FLEX"))  # initial protocol header
-        cmd = {"cmd": "AUTH", "payload": [username]}
+        cmd = {"cmd": "AUTH", "payload": [self.pub_key]}
         self.send_datum(cmd, Datum.Command)
 
         _, challenge_d = self.read_datum()
         print(challenge_d)
-        self.send_auth_response(challenge_d[b"challenge"])
+        self.send_auth_response(challenge_d["challenge"])
         self.request_roster()
-        _, _ = self.read_datum() # user datum
-        _, self.roster = self.read_datum() # roster
-        #print(d_type, d_data)
+        _, _ = self.read_datum()  # user datum?
+
+        Thread(target=self.mainloop).start()
 
     def mainloop(self):
         while True:
-            d_type, d_data = self.read_datum()
+            try:
+                d_type, d_data = self.read_datum()
+            except Exception as e:
+                print(e)
+                continue
             print(d_type, d_data)
             if d_type == Datum.Roster:
                 self.roster = d_data
+            if d_type == Datum.Message:
+                self.got_message_callback(d_data)
+
+    def got_message_callback(self, d_data):
+        pass
 
     def request_roster(self):
-        self.send_datum({b"cmd": "ROSTER"}, Datum.Command)
+        self.send_datum({"cmd": "ROSTER"}, Datum.Command)
 
-    def send_auth_response(self, challenge):
-        self.send_datum({b"challenge": challenge}, Datum.AuthResponse)
+    def send_auth_response(self, challenge: str):
+        self.send_datum({"challenge": challenge}, Datum.AuthResponse)
 
     def send_message(
-        self, to, category="test", flags=["FLAGS?"], date=dt.now(), message=""
+        self,
+        to: str,
+        category: str = "test",
+        flags: Optional[List[str]] = ["FLAGS?"],
+        message: str = "",
     ):
+        to = hexlify(bytes(to, encoding="utf-8"))
+
         msg = {
-            b"to": to,
-            b"from": self.username,
-            b"flags": flags,
-            b"date": date,
-            b"msg": msgstr,
+            "to": to,
+            "from": self.username,
+            "flags": flags,
+            "date": int(dt.now().timestamp()),
+            "msg": message,
         }
-        d_type = Datum.Message  # type 3 = msg
+        d_type: Datum = Datum.Message  # type 3 = msg
         self.send_datum(msg, d_type)
 
-    def send_datum(self, msg, d_type=0):
-        out = msgpack.packb(msg, encoding="utf-8")
-        size = len(out)
+    def send_datum(self, msg: str, d_type: Datum):
+        out: List[bytes] = msgpack.packb(msg, encoding="utf-8")
+        size: int = len(out)
         self.sock.send(
             (d_type.value).to_bytes(1, "big") + size.to_bytes(2, "big") + out
         )
 
     def read_datum(self):
-        d_type = int.from_bytes(self.sock.recv(1), "big")  # datum type
-        d_size = int.from_bytes(self.sock.recv(2), "big")  # length of msgpack data
-        datum = msgpack.unpackb(
-            self.sock.recv(d_size)
-        )  # TODO: proper exception handling
-        return (Datum(d_type), datum)
+        try:
+            d_type = Datum(int.from_bytes(self.sock.recv(1), "big"))  # datum type
+            d_size = int.from_bytes(self.sock.recv(2), "big")  # length of msgpack data
+            datum = msgpack.unpackb(
+                self.sock.recv(d_size), encoding="utf-8"
+            )  # TODO: proper exception handling
+            return (d_type, datum)
+        except Exception as e:
+            print(e)
